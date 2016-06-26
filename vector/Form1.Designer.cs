4c506bbdfee54613d821fb8f05c2466329e4ec79
yhorby matias <yhorby matias>
2016-06-25 22:14:08 -0400

    Uploading source code vector C#

---
+++ vector/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace vector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_plano = new System.Windows.Forms.Button();
+            this.btn_vector = new System.Windows.Forms.Button();
+            this.btn_vectorabjo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_plano
+            // 
+            this.btn_plano.Location = new System.Drawing.Point(12, 12);
+            this.btn_plano.Name = "btn_plano";
+            this.btn_plano.Size = new System.Drawing.Size(95, 23);
+            this.btn_plano.TabIndex = 0;
+            this.btn_plano.Text = "Dibujar plano";
+            this.btn_plano.UseVisualStyleBackColor = true;
+            this.btn_plano.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_vector
+            // 
+            this.btn_vector.Location = new System.Drawing.Point(12, 41);
+            this.btn_vector.Name = "btn_vector";
+            this.btn_vector.Size = new System.Drawing.Size(95, 23);
+            this.btn_vector.TabIndex = 1;
+            this.btn_vector.Text = "Dibujar vector";
+            this.btn_vector.UseVisualStyleBackColor = true;
+            this.btn_vector.Click += new System.EventHandler(this.btn_vector_Click);
+            // 
+            // btn_vectorabjo
+            // 
+            this.btn_vectorabjo.Location = new System.Drawing.Point(12, 70);
+            this.btn_vectorabjo.Name = "btn_vectorabjo";
+            this.btn_vectorabjo.Size = new System.Drawing.Size(95, 23);
+            this.btn_vectorabjo.TabIndex = 2;
+            this.btn_vectorabjo.Text = "Dibujar vector";
+            this.btn_vectorabjo.UseVisualStyleBackColor = true;
+            this.btn_vectorabjo.Click += new System.EventHandler(this.btn_vectorabjo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 441);
+            this.Controls.Add(this.btn_vectorabjo);
+            this.Controls.Add(this.btn_vector);
+            this.Controls.Add(this.btn_plano);
+            this.Name = "Form1";
+            this.Text = "Vector";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_plano;
+        private System.Windows.Forms.Button btn_vector;
+        private System.Windows.Forms.Button btn_vectorabjo;
+    }
+}
+
---
+++ vector/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // aqui llenamos las variables de la linea vertical
+            int x1v = 250;
+            int x2v = 250;
+            int y1v = 0;
+            int y2v = 500;
+            //fin
+
+            // aqui llenamos las variable de la linea horizontal
+            int x1h = 0;
+            int x2h = 500;
+            int y1h = 250;
+            int y2h = 250;
+            // fin
+
+            Graphics grafico; // llamamos a Graphics que es la que nos permitira dibujar
+            grafico = this.CreateGraphics();
+            Pen lapiz;       // llamamos a Pen que es un objeto que nos permite dibujar lineas y curvas
+            lapiz = new Pen(Color.Chocolate, 5);
+            grafico.DrawLine(lapiz, x1v, y1v, x2v, y2v); // Utilizamos el metodo DrawLine para dibujar las lineas verticales
+
+            grafico.DrawLine(lapiz, x1h, y1h, x2h, y2h);  // Utilizamos el metodo DrawLine para dibujar las lineas horizontales
+        }
+
+        private void btn_vector_Click(object sender, EventArgs e)
+        {
+            // aqui llenamos las variable de la linea del vector
+            int x1v = Width/ 2; // le decimos posicionate a la mitad de la anchura, para que quede dentro del plano
+            int x2v = 500;
+            int y1v = Height / 2; // le decimos posicionate a la mitad de la altura, para que quede dentro del plano
+            int y2v = 0;
+            Graphics grafico;
+            grafico = this.CreateGraphics();
+            Pen lapiz;
+            lapiz = new Pen(Color.Black, 5); // aqui definimos de que color queremos la linea y el grosor de la misma
+            grafico.DrawLine(lapiz, x1v, y1v, x2v, y2v);
+        }
+
+        // se repite igual a partir de aqui ---->
+        private void btn_vectorabjo_Click(object sender, EventArgs e)
+        {
+            int x1v = Width / 2;
+            int x2v = 400;
+            int y1v = Height / 2;
+            int y2v = 0;
+            Graphics grafico;
+            grafico = this.CreateGraphics();
+            Pen lapiz;
+            lapiz = new Pen(Color.BlueViolet, 5);
+            grafico.DrawLine(lapiz, x1v, y1v, x2v, y2v);
+
+
+        }
+    }
+}
