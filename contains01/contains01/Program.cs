82b3f3c9dbcefdecfb62d9292adacaa44d16761c
yhorby matias <yhorby matias>
2016-06-27 18:19:06 -0400

    fixing error code

--- contains01/contains01/Program.cs
+++ contains01/contains01/Program.cs
@@ -1,40 +1,20 @@
 ﻿using System;
+using System.Linq;
 
-class Program
+namespace contains01
 {
-    static void Main()
+    class Program
     {
-        Test("Dot Net Perls");
-        Test("dot net perls");
-    }
-
-    static void Test(string input)
-    {
-        Console.Write("--- ");
-        Console.Write(input);
-        Console.WriteLine(" ---");
-        //
-        // See if the string contains 'Net'
-        //
-        bool contains = input.Contains("Net");
-        //
-        // Write the result
-        //
-        Console.Write("Contains 'Net': ");
-        Console.WriteLine(contains);
-        //
-        // See if the string contains 'perls' lowercase
-        //
-        if (input.Contains("perls"))
+        static void Main(string[] args)
         {
-            Console.WriteLine("Contains 'perls'");
-        }
-        //
-        // See if the string contains 'Dot'
-        //
-        if (!input.Contains("Dot"))
-        {
-            Console.WriteLine("Doesn't Contain 'Dot'");
+            //declaramos la cadena
+            string cadena = "HOLA PROGRAMADORES .NET EN ESPANOL";
+            Console.WriteLine(" \n TRABAJANDO CON CADENAS (CONTAINS) \n") ;
+            //imprimimos la cadena
+            Console.WriteLine(" \n Nuestra cadena es: " + cadena + "\n");
+           
+            //buscamos en  la cadena
+            Console.WriteLine(" \n La cadena contiene la letra P ?:{0}", cadena.Contains('P'));
             Console.ReadKey();
         }
     }
