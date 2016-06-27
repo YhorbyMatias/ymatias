fc92b8ce8326e12532b79fca4abc7abf00923b32
yhorby matias <yhorby matias>
2016-06-27 16:20:24 -0400

    uploading

---
+++ contains01/contains01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace contains01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaramos la cadena
+            string cadena = "HOLA PROGRAMADORES .NET EN ESPANOL";
+            Console.WriteLine(" \n TRABAJANDO CON CADENAS (CONTAINS) \n") ;
+            //imprimimos la cadena
+            Console.WriteLine(" \n Nuestra cadena es: " + cadena + "\n");
+           
+            //buscamos en  la cadena
+            Console.WriteLine(" \n La cadena contiene la letra P ?:{0}", cadena.Contains('P'));
+            Console.ReadKey();
+        }
+    }
+}
