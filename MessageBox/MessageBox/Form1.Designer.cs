eddc89b4be87b0d4f77fad094b9a24504f87834e
yhorby matias <yhorby matias>
2016-07-03 15:55:27 -0400

    uploading source code tutorial MessageBox

---
+++ MessageBox/MessageBox/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace MessageBox01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bntNormal = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txt_num1 = new System.Windows.Forms.TextBox();
+            this.txt_num2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bntNormal
+            // 
+            this.bntNormal.Location = new System.Drawing.Point(29, 28);
+            this.bntNormal.Name = "bntNormal";
+            this.bntNormal.Size = new System.Drawing.Size(75, 23);
+            this.bntNormal.TabIndex = 0;
+            this.bntNormal.Text = "Normal";
+            this.bntNormal.UseVisualStyleBackColor = true;
+            this.bntNormal.Click += new System.EventHandler(this.bntNormal_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Con titulo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(223, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Con Botones";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 71);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Con icono";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(388, 71);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(161, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Mostrar resultado Suma";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // txt_num1
+            // 
+            this.txt_num1.Location = new System.Drawing.Point(388, 30);
+            this.txt_num1.Name = "txt_num1";
+            this.txt_num1.Size = new System.Drawing.Size(100, 20);
+            this.txt_num1.TabIndex = 5;
+            // 
+            // txt_num2
+            // 
+            this.txt_num2.Location = new System.Drawing.Point(511, 30);
+            this.txt_num2.Name = "txt_num2";
+            this.txt_num2.Size = new System.Drawing.Size(100, 20);
+            this.txt_num2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(492, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "+";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 117);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_num2);
+            this.Controls.Add(this.txt_num1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bntNormal);
+            this.Name = "Form1";
+            this.Text = "MessageBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bntNormal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txt_num1;
+        private System.Windows.Forms.TextBox txt_num2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MessageBox/MessageBox/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageBox01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bntNormal_Click(object sender, EventArgs e)
+        {
+            //muestra un mensaje normal
+            MessageBox.Show("Hola Programador");
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Muestra un mensaje con título
+            MessageBox.Show("Hola Programador", "Aqui titulo del mensaje");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Muestra un mensaje con mas botones ademas que el OK (aceptar)
+            MessageBox.Show("Hola Programador", "Aqui titulo del mensaje",MessageBoxButtons.YesNoCancel);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Mostrar un mensaje con ícono
+            MessageBox.Show("Hola Programador", "Aqui titulo del mensaje", MessageBoxButtons.YesNoCancel,MessageBoxIcon.Exclamation);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //aqui mostramos variables en el MessageBox
+            //declaramos una variable entera para el valor 2
+            //y asignamos su valor convirtiendo a entero
+           
+            int numero2 = Convert.ToInt32(txt_num2.Text);
+            //se declara una variable entera para la suma y se realiza la misma
+            int suma = Convert.ToInt32(txt_num1.Text) + numero2;
+            //Mostrar el resultado de la suma
+            MessageBox.Show("La suma de "+ Convert.ToInt32(txt_num1.Text) + " + " + numero2 + " es: " + suma + "", "Resultado de la suma", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
